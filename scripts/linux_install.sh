1136f38a75d42fc436b7bbee7d024b8999ec6319
joncrall <jon.crall@kitware.com>
2022-07-22 16:48:32 -0400

    Script with instructions for setup on linux

--- scripts/linux_install.sh
+++ scripts/linux_install.sh
@@ -8,3 +8,9 @@ pyuic6 './res/about.ui' -o './src/gen/about.py'
 pyuic6 './res/design.ui' -o './src/gen/design.py'
 pyuic6 './res/settings.ui' -o './src/gen/settings.py'
 pyuic6 './res/update_checker.ui' -o './src/gen/update_checker.py'
+
+BUILD_NUMBER=$(date +'%Y%m%d%H%M%S')
+echo "AUTOSPLIT_BUILD_NUMBER = '$BUILD_NUMBER'" > src/gen/build_number.py
+
+# Run the program
+(cd src && python AutoSplit.py)
